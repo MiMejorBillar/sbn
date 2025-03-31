b26455363260717a9445e8c5734e740e808bafd5
Luis Fernando Carrasco Orihuela <mimejorbillar@gmail.com>
2025-03-31 01:16:25 -0700

    preset players added

--- lib/riverpod/providers.dart
+++ lib/riverpod/providers.dart
@@ -24,14 +24,20 @@ class Player {
   final String icon; 
   Player({required this.name, required this.handicap, required this.icon});
 }
-
+final List<Player> presetPlayers = [
+  Player(name: 'Luis', handicap: 20, icon: 'assets/flags/peru.png'),
+  Player(name: 'Seddy Merckx', handicap: 18, icon: 'assets/flags/canada.png'),
+  Player(name: 'Kyungoh', handicap: 22, icon: 'assets/flags/korea.png'),
+  Player(name: 'Shinpata', handicap: 18, icon: 'assets/flags/korea.png'),
+  Player(name: 'Jongnetti', handicap: 15, icon: 'assets/flags/korea.png'),
+];
 final playersProvider =
     StateNotifierProvider<PlayersNotifier, List<Player>>((ref) {
   return PlayersNotifier();
 });
 
 class PlayersNotifier extends StateNotifier<List<Player>> {
-  PlayersNotifier() : super([]);
+  PlayersNotifier() : super(presetPlayers);
 
   void addPlayer(Player player) {
     state = [...state, player];
